16887c0f381478e065907bd6b3189b3566882411
nishanth1991 <nishanthg1991@gmail.com>
2014-04-19 05:22:41 -0400

    CHanges

--- EAMMH.m
+++ EAMMH.m
@@ -1,17 +1,4 @@
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%                                                                      %
-% This is the LEACH [1] code we have used.                             %
-% The same code can be used for FAIR if m=1                            %
-%                                                                      %
-% [1] W.R.Heinzelman, A.P.Chandrakasan and H.Balakrishnan,             %
-%     "An application-specific protocol architecture for wireless      % 
-%      microsensor networks"                                           % 
-%     IEEE Transactions on Wireless Communications, 1(4):660-670,2002  %
-%                          www.forum.wsnlab.ir                         %                    
-%     Homaei@wsnlab.ir &  Farhadi@wsnlab.ir & Ranjbaran@wsnlab.ir      %
-%                                                                      %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear;
 
